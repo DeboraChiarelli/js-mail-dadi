aac6d9c310d65754662dc396caf1d253807633d4
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-09 19:51:49 +0200

    Add - control condition

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 //# Mail
 //- Chiedi all’utente la sua email 
 //    - dichiaro l'array
-const mailList = ["mail00@gmail.com", "mail01@gmail.com", "mail02@gmail.com", "mail03@gmail.com", "mail04@gmail.com"];
+const mailLists = ["mail00@gmail.com", "mail01@gmail.com", "mail02@gmail.com", "mail03@gmail.com", "mail04@gmail.com"];
+console.log(mailLists)
 //    - assegno alla variabile il valore costituito da un prompt
 const promptMail = prompt ("Inswerisci la tua email", "mail00@gmail.com");
 //- controllo che sia nella lista di chi può accedere 
 //    - creo una variabile di controllo falsa, con accesso negato
 let control = false;
 //    - creo un ciclo per cercare l'email nell'indice
-for (let i = 0; i < mailList.length; i++) {
-
+for (let i = 0; i < mailLists.length; i++) {
+//    - SE è presente 
+      if (mailLists[i] === promptMail) {
+        control = true;                                   // ALLORA cambio il valore di control 
+      }
 }
+console.log(control);
+//- SE è true 
+if (control) {
+    console.log(`${promptMail} è presente il lista`)    // ALLORA sarà presente il lista 
+} else {
+    console.log(`${promptMail} non è presente in lista`) // ALTRIMENTI non sarà presente in lista 
+}
+
+
 //- stampa un messaggio appropriato sull’esito del controllo.
--- readme.md
+++ readme.md
@@ -5,5 +5,11 @@
 - controllo che sia nella lista di chi può accedere: 
     - creo una variabile di controllo falsa, con accesso negato
     - creo un ciclo per cercare l'email nell'indice
+        - SE  è presente 
+          cambio il valore di control
+        - controllo fuori dal ciclo per non avere messaggi continui se control è true o false
+            - SE è true 
+              ALLORA sarà presente il lista 
+              ALTRIMENTI non sarà presente in lista 
 - stampa un messaggio appropriato sull’esito del controllo.
 
